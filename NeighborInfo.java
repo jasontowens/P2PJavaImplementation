ff14c6cafcbc359a52d54858f96764b159c0d434
Brian Basnight <bbasnight@Brians-MacBook-Pro.local>
2016-10-18 23:29:43 -0400

    now parses all PeerInfo and correctly puts them in _neighborInfos, an ArrayList for retaining data about neighbors

--- NeighborInfo.java
+++ NeighborInfo.java
@@ -4,23 +4,38 @@
 */
 
 class NeighborInfo{
-	private byte[] _bitfield;
+	public int _peerID;
+	public String _hostName;
+	public int _portNum;
+	public boolean _fullFile;
+	public int _speed;
+	public byte[] _bitfield;
 	public boolean _isOUneighbour; 	// optimistically unchoked
 	public boolean _choked;
 	public boolean _interested;
 	
 
-	public int _peerID;
-	public int _speed;
 
-	public NeighborInfo(){
-		// 1000 is a placeholder, will need to take from cfg file
-		_bitfield = new byte[1000];
+
+	public NeighborInfo(int peerID, String hostName, 
+						int portNum, int fileSize, boolean fullFile){
+
+		_peerID = peerID;
+		_hostName = hostName;
+		_portNum = portNum;
+		_bitfield = new byte[fileSize];
+		_fullFile = fullFile;
+
+		System.out.println("---------new NeighborInfo created.\n\t\t_peerID = " + _peerID + "\n\t\t_hostName = " + hostName
+							+ "\n\t\t_portNum = " + _portNum + "\n\t\t_bitfield size = " + fileSize + "\n\t\t_fullFile = " + _fullFile
+							+ "\n--------- end of new NeighborInfo creation.");
+
 	}
 
 	public void setPeerID(int newID){
 		_peerID = newID;
 	}
+
 	public int getPeerID(){
 		return _peerID;
 	}
--- peerProcess.java
+++ peerProcess.java
@@ -16,34 +16,42 @@ class peerProcess{
 	private String _fileName;
 	private int _fileSize;
 	private int _pieceSize;
+	private int _numPieces;
 
 
 	public peerProcess(int peerID){
 		_peerID = peerID;
-		// 1000 is a placeholder, will need to take from cfg file
-		_bitfield = new byte[1000];
+		_neighborInfos = new ArrayList<NeighborInfo>();
 	}
 
 	public peerProcess() {
-		_bitfield = new byte[1000];
+		_neighborInfos = new ArrayList<NeighborInfo>();
 	}
 
 	public void initialize(int peerID) {
 		_peerID = peerID;
+
+		// all in one method for parsing the common config file
 		parseCommonConfig("Common.cfg");
 
+		// kept out of the function kind of unnecessarily, but fn would still work
+		// if out of order config files were expected
+		_bitfield = new byte[_fileSize];
+		_numPieces = _fileSize / _pieceSize;
 
+		parsePeerConfig("peer_" + _peerID + "/PeerInfo.cfg");
 
 		System.out.println("At bottom of initialize(): _peerID = " + _peerID);
 		System.out.println("At bottom of initialize(): _numPreferredNeighbors = " + _numPreferredNeighbors);
-		System.out.println("At bottom of initialize(): _numPreferredNeighbors = " + _unchokingInterval);
-		System.out.println("At bottom of initialize(): _numPreferredNeighbors = " + _ouInterval);
-		System.out.println("At bottom of initialize(): _numPreferredNeighbors = " + _fileName);
-		System.out.println("At bottom of initialize(): _numPreferredNeighbors = " + _fileSize);
-		System.out.println("At bottom of initialize(): _numPreferredNeighbors = " + _pieceSize);
+		System.out.println("At bottom of initialize(): _unchokingInterval = " + _unchokingInterval);
+		System.out.println("At bottom of initialize(): _ouInterval = " + _ouInterval);
+		System.out.println("At bottom of initialize(): _fileName = " + _fileName);
+		System.out.println("At bottom of initialize(): _fileSize = " + _fileSize);
+		System.out.println("At bottom of initialize(): _pieceSize = " + _pieceSize);
+		System.out.println("At bottom of initialize(): _neighborInfos.size() = " + _neighborInfos.size());
 	}
 
-	// don't think we should be able to setPeerID after it is constructed
+	// don't think we should be able to setPeerID after it is constructed -> commented out for now
 	// public void setPeerID(int newID){
 	// 	_peerID = newID;
 	// }
@@ -60,9 +68,11 @@ class peerProcess{
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(configFileName));
 		    String line;
+		    // if theres still a line left keep going
 		    while ((line = br.readLine()) != null) {
+		    	//split line at every blank space
 		    	String [] splitLine = line.split("\\s+");
-				// System.out.println(line);
+				//decide what to do on what the particular split part is 
 	    	    switch (splitLine[0]) {
 
 			        case "NumberOfPreferredNeighbors":
@@ -91,16 +101,50 @@ class peerProcess{
 
 			        default:
 			            throw new IllegalArgumentException("Invalid Common.cfg field: " + splitLine[0]);
-		    	}
-	    	}
-	    }
+
+		    	} // end switch
+	    	} // end while
+	    } // end try
 	    catch (IOException e) {
 	    	System.out.println(e);
 	    	System.out.println("Invalid or not found Common.cfg");
-	    }
-	}
+	    } // end catch
+	} // end parseCommonConfig
+
+	private void parsePeerConfig(String peerInfoFileName) {
+
+		int neighborID;
+		String neighborHostName;
+		int neighborPortNum;
+		boolean neighborFullFile;
+		try {
+
+			BufferedReader br = new BufferedReader(new FileReader(peerInfoFileName));
+		    String line;
+		    while ((line = br.readLine()) != null) {
+
+		    	String [] splitLine = line.split("\\s+");
+				
+		    	// get variables ready to create a NeighborInfo - kind of unnecessary
+				neighborID = Integer.parseInt(splitLine[0]);
+				neighborHostName = splitLine[1];
+				neighborPortNum = Integer.parseInt(splitLine[2]);
+				neighborFullFile = ( Integer.parseInt(splitLine[3]) != 0 ); // if its 0 -> false  else -> true
+				
+				NeighborInfo ni = new NeighborInfo(neighborID, neighborHostName, 
+										neighborPortNum, _fileSize, neighborFullFile);
+	    	    _neighborInfos.add(ni);
+	    	} // end while
+
+	    } // end try
+	    catch (IOException e) {
+	    	System.out.println(e);
+	    	System.out.println("Invalid or not found PeerInfo.cfg");
+	    } // end catch 
+	} // end function
 
 	public static void main(String[] args) {
+
 		int peerID;
 		try {
 			peerID = Integer.parseInt(args[0]);
@@ -111,11 +155,9 @@ class peerProcess{
 			return;
 		}
 		
+		// make new peer process
 		peerProcess p = new peerProcess();
-
+		// feed it its peerID, and it will know the rest from there
 		p.initialize(peerID);
-
-
 	}
-
 }
